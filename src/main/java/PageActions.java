b621b054e334b0f637a61b9d411a5ccaf93d29e1
Chris Ryan <chrisrryan@gmail.com>
2022-12-01 22:06:34 +0000

    Remove shadow DOM and Aria

--- src/main/java/PageActions.java
+++ src/main/java/PageActions.java
@@ -8,7 +8,7 @@ import static java.lang.Thread.sleep;
 public class PageActions {
 
     private ChromeDriver driver;
-    private SearchContext keyboardShadowRoot;
+    private WebElement keyboard;
     private WebElement boardElement;
     private Solver solver;
 
@@ -25,26 +25,11 @@ public class PageActions {
         driver.get(baseUrl);
 
         // Close the GDPR dialogue
-        driver.findElement(By.id("pz-gdpr-btn-accept")).click();
+        driver.findElement(By.id("pz-gdpr-btn-closex")).click();
+        // Close How to play
+        driver.findElement(By.xpath("//button[contains(@aria-label, 'Close')]")).click();
 
-        // Locate and store the game shadow dom element
-        WebElement shadowHost = driver.findElement(By.tagName("game-app"));
-        SearchContext shadowRoot = shadowHost.getShadowRoot();
-        WebElement gameShadowContent = shadowRoot.findElement(By.cssSelector("#game"));
-
-        // Locate and store the keyboard shadow dom element to use throughout play
-        shadowHost = gameShadowContent.findElement(By.tagName("game-keyboard"));
-        keyboardShadowRoot = shadowHost.getShadowRoot();
-        // Locate and store the results board element to use throughout play
-        boardElement = gameShadowContent.findElement(By.id("board"));
-
-        // Close the game information modal
-        shadowHost = gameShadowContent.findElement(By.tagName("game-modal"));
-        shadowRoot = shadowHost.getShadowRoot();
-        shadowRoot.findElement(By.cssSelector(".close-icon")).click();
-
-        // Wait for the cookies dialogue to go. It's un-closable and covers part of the keyboard.
-        wait(10000);
+        keyboard = driver.findElement(By.xpath("//div[contains(@aria-label, 'Keyboard')]"));
     }
 
     public boolean enterWord() {
@@ -53,7 +38,7 @@ public class PageActions {
         for (int i = 0; i < 5; i++) clickLetter(word.charAt(i));
         clickLetter('↵'); // Enter key
         // Wordle doesn't reveal word results immediately. Give it time to do its thing.
-        wait(3000);
+        wait(2000);
 
         int attempts = solver.getAttempts();
         String[] evaluation = new String[5];
@@ -64,14 +49,13 @@ public class PageActions {
     }
 
     private String letterEvaluation(int rowIndex, int tileIndex) {
-        WebElement row = boardElement.findElement(By.cssSelector(String.format("game-row:nth-child(%s)", rowIndex)));
-        SearchContext shadowRoot = row.getShadowRoot();
-        WebElement tile = shadowRoot.findElement(By.cssSelector(String.format("game-tile:nth-child(%s)", tileIndex)));
-        return tile.getAttribute("evaluation");
+        String tileXPath = "//div[contains(@aria-label, 'Row " + rowIndex + "')]/div[" + tileIndex + "]/div";
+        WebElement tile = driver.findElement(By.xpath(tileXPath));
+        return tile.getAttribute("data-state");
     }
 
     private void clickLetter(char letter) {
-        keyboardShadowRoot.findElement(By.cssSelector(String.format("button[data-key='%s']", letter))).click();
+        keyboard.findElement(By.cssSelector(String.format("button[data-key='%s']", letter))).click();
     }
 
     public void wait(int ms) {
